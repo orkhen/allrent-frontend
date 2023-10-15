f3a137fbc0e2e6a8d4a2ba446e7bce0569f4e63c
Orkhan Aliyev <orkhan1aliyev@gmail.com>
2023-10-15 09:19:57 +0200

    'Updated'

--- src/components/PropertyPost/PropertyPostCalendar/PropertyPostCalendar.jsx
+++ src/components/PropertyPost/PropertyPostCalendar/PropertyPostCalendar.jsx
@@ -5,19 +5,32 @@ import * as locales from 'react-date-range/dist/locale';
 import { Calendar } from 'react-date-range';
 
 const PropertyPostCalendar = ({ step, switchStep }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  
   const [locale, setLocale] = useState('en');
   
   var today = new Date();
-
+  
   const [date, setDate] = useState(null);
-
+  
   useEffect(() => {
     if (step === 6) {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     }
-    }, [step]);
+  }, [step]);
+  
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-  console.log(date)
+  // console.log(date)
   return (
     <div className={`property-post-calendar d-flex justify-content-center ${step !== 6 ? 'hide' : 'slide-top'}`}>
       <div className="property-post-calendar-container property-post-container d-flex flex-column mb-5">
@@ -30,7 +43,7 @@ const PropertyPostCalendar = ({ step, switchStep }) => {
             onChange={item => setDate(item)}
             locale={locales[locale]} 
             date={date} 
-            months={2}
+            months={windowWidth > 768 ? 2 : 1}
             direction='horizontal'
             minDate={today}
             color='#fe4343'
